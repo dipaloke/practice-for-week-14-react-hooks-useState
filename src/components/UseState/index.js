a887a2148165ccf9b2b9b2656fbba265de63d395
Dipaloke Biswas <dipalokebiswas96@gmail.com>
2024-05-19 12:15:26 +0600

    Initial commit

---
+++ src/components/UseState/index.js
@@ -0,0 +1,30 @@
+import "./UseState.css";
+import { useState } from "react";
+
+const UseState = () => {
+  const [theme, setTheme] = useState("light");
+  const [count, setCount] = useState(0);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <div className={`state ${theme}`}>
+      <h1>UseState Component</h1>
+      <button onClick={toggleTheme}>
+        {theme === "light" ? "Dark" : "Light"}
+      </button>
+      <h2>DISPLAY COUNT HERE</h2>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+        Decrement
+      </button>
+    </div>
+  );
+};
+
+export default UseState;
